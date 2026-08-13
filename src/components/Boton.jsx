580bb4f3bebe7b5db372a5bb709a93832381ab88
Shaihueque <shaihueque@gmail.com>
2026-08-13 12:14:16 -0300

    se limpiaron archivos y cosas de mas

--- src/components/Boton.jsx
+++
@@ -1,8 +0,0 @@
-
-import Styles from "./modulesCSS/Boton.module.css"
-
-function Boton ({label, variant, onClick}){
-    return <button className={`${Styles.btnG} ${Styles[variant]}`} onClick={onClick}>{label} </button>
-}
-
-export default Boton
--- src/components/Button.jsx
+++
@@ -1,7 +0,0 @@
-import Styles from './modulesCSS/button.module.css'
-
-function Button ({label, variant, onClick}){
-    return <button className={`${Styles.btnG} ${Styles[variant]}`} onClick={onClick}>{label} </button>
-}
-
-export default Button
